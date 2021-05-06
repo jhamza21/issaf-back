5db1dc7bf0a581db34e529404d0df95b72113c86
Hamza <j.hamza@hotmail.fr>
2021-05-06 02:57:01 +0100

    fix heroku

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -55,12 +55,23 @@ class LoginController extends Controller
         if ($this->attemptLogin($request)) {
             $user = $this->guard()->user();
             $user->generateToken();
-    
+
             return response()->json([
                 'data' => $user->toArray(),
             ]);
         }
-    
+
         return $this->sendFailedLoginResponse($request);
+    }
+    public function logout(Request $request)
+    {
+        $user = Auth::guard('api')->user();
+
+        if ($user) {
+            $user->api_token = null;
+            $user->save();
+        }
+
+        return response()->json(['data' => 'User logged out.'], 200);
     }
 }
